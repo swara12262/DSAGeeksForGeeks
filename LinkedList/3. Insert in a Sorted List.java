b87b15402972d8d45a787aba0c4ad7c3d40e833f
swara12262 <sawant12262@gmail.com>
2020-10-02 16:55:29 +0530

    Create 3. Insert in a Sorted List.java

---
+++ LinkedList/3. Insert in a Sorted List.java
@@ -0,0 +1,87 @@
+// { Driver Code Starts
+
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head, int key) {
+        // Add your code here.
+        Node cur=head;
+        Node prev=null;
+        while(cur!=null&& cur.data< key){
+            prev=cur;
+            cur=cur.next;
+        }
+        Node newNode=new Node(key);
+        if(prev==null){
+            newNode.next=head;
+            head=newNode;
+        }
+        else{
+            prev.next=newNode;
+            newNode.next=cur;
+        }
+        return head;
+        
+    }
+}
